d3ad315d1c1a80a2557a24bc33c771b7b2d8556f
FancyPupper <A01274757@itesm.mx>
2020-06-10 16:37:58 -0500

    Alerts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClientModule, HttpClient, HttpHeaders} from '@angular/common/http';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 4000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
 
 interface Course {
   description: string;
@@ -35,8 +48,15 @@ export class AuthService {
       if (resp.mensaje =="Usuario o contraseña incorrectos" ) {
         // si la api regresa el mensaje "Usuario o contraseña incorrectos"
         console.log("Error de authenticacion");
-        alert("Usuario o contraseña incorrectos");
-        window.location.reload();
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Contraseña o usuario incorrectos',
+        })
+
+        //alert("Usuario o contraseña incorrectos");
+        //window.location.reload();
     }else if(resp.mensaje =="Autenticación correcta"){
       // si la api regresa el mensaje "Autenticación correcta"
       console.log(resp);
@@ -44,6 +64,11 @@ export class AuthService {
       localStorage.setItem('auth_token', resp.token);
       this.router.navigate(['home']);
       window.location.reload();
+
+      Toast.fire({
+        icon: 'success',
+        title: 'Inicio de sesión exitoso'
+      })
       
     }
 
